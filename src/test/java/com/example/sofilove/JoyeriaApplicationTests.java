1def9742c03fde8329f52811f898be38452969dd
hanksvi <hanks.vargas@utec.edu.pe>
2025-01-16 18:42:19 -0500

    reinicio

---
+++ src/test/java/com/example/sofilove/JoyeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sofilove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
